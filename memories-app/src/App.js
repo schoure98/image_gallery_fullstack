ce85bff47e7eb6867895bd986ebd1b6613f9453b
Rutuja Padgilwar <rutujapadgilwar@Rutujas-MacBook-Pro.local>
2022-11-11 00:49:48 -0800

    firebase configuration and connectivity

--- memories-app/src/App.js
+++ memories-app/src/App.js
@@ -1,12 +1,36 @@
+import { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { db } from "./firebase_conf";
+import { collection, getDocs } from "firebase/firestore";
+import { async } from "@firebase/util";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+      console.log(data.docs);
+
+    };
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        Memories: The digital souvenir 
-      </header>
+      <header>Memories - The Digital Souvenir</header>
+      {users.map((user) => {
+        return(
+          <div>
+            {" "}
+            <h1> Name: {user.name}</h1>
+            <h1> Age: {user.age}</h1> 
+          </div>
+        );
+      })}
     </div>
   );
 }
