e455a0223f9d5fc47cc2da423fb8efd96858ed8a
schoure98 <schoure@pdx.edu>
2022-11-17 13:39:32 -0800

    Fullstack project

--- memories-app/src/App.js
+++ memories-app/src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
+import About from "./Components/About";
 import FloatingButton from "./Components/FloatingButton";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+
 import SearchBar from "./Components/SearchBar";
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <SearchBar />
+      <About />
       <FloatingButton />
       <Footer />
     </div>
--- memories-app/src/Components/Header.js
+++ memories-app/src/Components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import background from "../background.jpeg";
 import logo from "../logo.png";
 
 function Header() {
   return (
     <div>
       <header>
-        <img src={logo} alt="logo" width="14%" />
+        <img src={background} alt="logo" width="14%" />
       </header>
     </div>
   );
