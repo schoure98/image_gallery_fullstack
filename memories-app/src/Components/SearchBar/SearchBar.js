c8969a12650067ff0f94b666742f453af7c2206b
schoure98 <schoure@pdx.edu>
2022-11-20 20:52:22 -0800

    adding functionality to search bar

--- memories-app/src/Components/SearchBar/SearchBar.js
+++ memories-app/src/Components/SearchBar/SearchBar.js
@@ -1,15 +1,37 @@
 import React from "react";
-import "../SearchBar/SearchBar.css"
+import "../SearchBar/SearchBar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
+import { useRef, useEffect } from "react";
+
 let element = <FontAwesomeIcon icon={faSearch} />;
 function SearchBar() {
+  const autoCompleteRef = useRef();
+  const inputRef = useRef();
+  const options = {
+    componentRestrictions: { country: "usa" },
+    fields: ["address_components", "geometry", "icon", "name"],
+    types: ["establishment"],
+  };
+  useEffect(() => {
+    autoCompleteRef.current = new window.google.maps.places.Autocomplete(
+      inputRef.current,
+      options
+    );
+  }, []);
+
   return (
     <div>
-        <form class="nosubmit">
-          <input class="nosubmit" type="search" placeholder="Search..." />
-        </form>
+      <form class="nosubmit">
+        <input
+          ref={inputRef}
+          class="nosubmit"
+          id="search_input"
+          type="search"
+          placeholder="Search..."
+        />
+      </form>
     </div>
   );
 }
